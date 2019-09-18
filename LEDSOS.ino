5f51c8b580c2edd9660035010c8f1de631a5694c
mohammedali452 <bilalmohd731@gmail.com>
2019-09-17 20:02:15 -0400

    Update LEDSOS.ino

--- LEDSOS.ino
+++ LEDSOS.ino
@@ -1,24 +1,27 @@
+int LED = 21;
+
 void setup() {
+  int LED = 21;
   // put your setup code here, to run once:
-  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(LED, OUTPUT);
 }
 
     void letterrs(){
-       digitalWrite(LED_BUILTIN, HIGH);   
-       delay(1000);                       // wait for a second
-       digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-       delay(1000); 
-       digitalWrite(LED_BUILTIN, HIGH);
-       delay(1000);
-        }
-        
+      for(int i=0; i<3; i++){
+        digitalWrite(LED, HIGH);   
+        delay(1000);
+        digitalWrite(LED, LOW);   
+        delay(1000);
+      }
+    }
+      
       void letterro() {
-       digitalWrite(LED_BUILTIN, HIGH);   
-       delay(3000);                       
-       digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-       delay(3000); 
-       digitalWrite(LED_BUILTIN, HIGH);
-       delay(3000);
+       for(int i=0; i<3; i++){
+        digitalWrite(LED, HIGH);   
+        delay(3000);
+        digitalWrite(LED, LOW);   
+        delay(3000);
+        }
       }
       
 void loop() {
