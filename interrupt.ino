1364763ca7676a4826d8cecebe80898f6cff73d1
sravell <55066770+sravell@users.noreply.github.com>
2019-09-20 19:37:24 -0400

    Update interrupt.ino

--- interrupt.ino
+++ interrupt.ino
@@ -2,35 +2,24 @@ int buttonPin = PC_13;
 int ledPin = LED_BUILTIN;
 volatile int buttonState = 0;
 
-void letterrs(){
-       digitalWrite(LED_BUILTIN, HIGH);   
-       delay(250);                       // wait for a second
-       digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-       delay(250); 
-       digitalWrite(LED_BUILTIN, HIGH);
-       delay(250);
-       digitalWrite(LED_BUILTIN, LOW);
-       delay(250);
-       digitalWrite(LED_BUILTIN, HIGH);
-       delay(250);
-       digitalWrite(LED_BUILTIN, LOW);
-       delay(250);
-        }
+void SLetter(){
+       for(int i = 0; i < 3; i++){
+              digitalWrite(LED_BUILTIN, HIGH);   
+              delay(250);                       // wait for a second
+              digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+              delay(250);
+       }
+}
         
-      void letterro() {
-       digitalWrite(LED_BUILTIN, HIGH);   
-       delay(750);                       
-       digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-       delay(250); 
-       digitalWrite(LED_BUILTIN, HIGH);
-       delay(750);
-       digitalWrite(LED_BUILTIN, LOW);
-       delay(250);
-       digitalWrite(LED_BUILTIN, HIGH);
-       delay(750);
-       digitalWrite(LED_BUILTIN, LOW);
-       delay(250);
+void OLetter() {
+       for(int i = 0; i < 3; i++){
+              digitalWrite(LED_BUILTIN, HIGH);   
+              delay(750);                       
+              digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+              delay(250); 
+            }
       }
+
 void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
   pinMode(PC_13, INPUT);
@@ -45,12 +34,13 @@ void loop() {
        delay(3000);
 }
 void pin_ISR(){
-if (buttonState == HIGH) {
-       for (x=0, x<10, x++)
-      digitalWrite(LED_BUILTIN, HIGH);
-      delay(100);                       
-      digitalWrite(LED_BUILTIN, LOW);    
-      delay(100);
-}
+       
+       if (buttonState == HIGH) {
+              for (x=0, x<10, x++)
+                     digitalWrite(LED_BUILTIN, HIGH);
+                     delay(100);                       
+                     digitalWrite(LED_BUILTIN, LOW);    
+                     delay(100);
+                                }
      
     }}
