8a0079fb360bf1b7d5c22f005e4aad36bc7ef0a8
sravell <55066770+sravell@users.noreply.github.com>
2019-09-20 19:00:13 -0400

    Add files via upload

---
+++ interrupt.ino
@@ -0,0 +1,53 @@
+int buttonPin = PC_13;
+int ledPin = LED_BUILTIN;
+volatile int buttonState = 0;
+
+void letterrs(){
+       digitalWrite(LED_BUILTIN, HIGH);   
+       delay(250);                       // wait for a second
+       digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+       delay(250); 
+       digitalWrite(LED_BUILTIN, HIGH);
+       delay(250);
+       digitalWrite(LED_BUILTIN, LOW);
+       delay(250);
+       digitalWrite(LED_BUILTIN, HIGH);
+       delay(250);
+       digitalWrite(LED_BUILTIN, LOW);
+       delay(250);
+        }
+        
+      void letterro() {
+       digitalWrite(LED_BUILTIN, HIGH);   
+       delay(750);                       
+       digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+       delay(250); 
+       digitalWrite(LED_BUILTIN, HIGH);
+       delay(750);
+       digitalWrite(LED_BUILTIN, LOW);
+       delay(250);
+       digitalWrite(LED_BUILTIN, HIGH);
+       delay(750);
+       digitalWrite(LED_BUILTIN, LOW);
+       delay(250);
+      }
+void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(PC_13, INPUT);
+  attachInterrupt(0, pin_ISR, CHANGE);
+}
+
+void loop() {
+  buttonState = digitalRead(buttonPin);
+       letterrs();   
+       letterro();
+       letterrs();
+       delay(3000);
+}
+void pin_ISR(){
+if (buttonState == HIGH) {
+      digitalWrite(LED_BUILTIN, HIGH);
+      delay(1000);                       
+      digitalWrite(LED_BUILTIN, LOW);    
+      delay(1000);
+    }}
