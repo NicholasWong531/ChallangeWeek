65d7630dc1db018d755002dda269ff1f152478d9
sravell <55066770+sravell@users.noreply.github.com>
2019-09-20 19:42:32 -0400

    Update interrupt.ino

--- interrupt.ino
+++ interrupt.ino
@@ -11,7 +11,7 @@ void SLetter(){
        }
 }
         
-void OLetter() {
+void oLetter() {
        for(int i = 0; i < 3; i++){
               digitalWrite(LED_BUILTIN, HIGH);   
               delay(750);                       
@@ -28,14 +28,14 @@ void setup() {
 
 void loop() {
   buttonState = digitalRead(buttonPin);
-       letterrs();   
-       letterro();
-       letterrs();
+       sLetter();   
+       oLetter();
+       sLetter();
        delay(3000);
 }
 void pin_ISR(){
        if (buttonState == HIGH) {
-              for (int i = 0, i < 10, i++)
+              for (int i = 0; i < 10; i++){
                      digitalWrite(LED_BUILTIN, HIGH);
                      delay(100);                       
                      digitalWrite(LED_BUILTIN, LOW);    
